6341ee3c1a42c5a6d45624b46848db7cec8feafd
Abdul8457 <abdulab0505@gmail.com>
2026-09-01 20:39:36 +0200

    Update test_alerts.py

--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -4,7 +4,15 @@ from src.gps.location import GPSLocation
 
 
 def test_alert_contains_event_and_location():
-    event = AccidentEvent("2026-01-01 12:00:00", 10, 4.2, 210.0)
+    event = AccidentEvent(
+        "2026-01-01 12:00:00",
+        10,
+        4.2,
+        210.0,
+        "medium",
+        3,
+    )
+
     location = GPSLocation(49.0069, 8.4037)
 
     message = create_alert_message(event, location)
